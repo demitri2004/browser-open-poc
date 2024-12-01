30f4a38afc19f694dff5d92ca80e938702e9cf43
dkarpov <dmitri.karpov@cardworks.com>
2024-12-01 05:53:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'browser4-poc',
+  webDir: 'www'
+};
+
+export default config;
