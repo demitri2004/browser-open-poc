9ef7f9278d3db95049ede65366470b6b4e4c91ee
dkarpov <dmitri.karpov@cardworks.com>
2024-12-01 06:52:17 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InAppBrowser} from "@ionic-native/in-app-browser/ngx";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [InAppBrowser, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import {InAppBrowserOptions} from "@ionic-native/in-app-browser";
+
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
+  openInAppBrowser() {
+    const url = 'https://brappbeta.giltravel.com/';
+    const options: InAppBrowserOptions = {
+      location: 'yes',
+      toolbar: 'yes',
+    };
 
+    const browser = this.iab.create(url, '_blank', options);
+
+    browser.on('loadstart').subscribe((event) => {
+      console.log('Browser loadstart:', event.url);
+
+      if (event.url.includes('close-app://')) {
+        browser.close();
+      }
+    });
+
+    browser.on('exit').subscribe(() => {
+      console.log('Browser closed.');
+    });
+  }
 }
